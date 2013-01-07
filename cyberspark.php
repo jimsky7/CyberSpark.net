56fa5e78976e54af15e84784b83d08289dc18dd5
JimSky7 <sky@cyberspark.net>
2013-01-07 01:46:09 -0800

    Define 'rotate' property and write code to implement it. Also fix obscure race condition related to log rotations.
    See documentation for new 'rotate' property which is separate from
    'sendlogs' property. Bug fixed: If daemon was shut down during the
    -notify- hour, then started in a later hour, logs would rotate every
    time around the loop.

--- cyberspark.php
+++ cyberspark.php
@@ -293,6 +293,15 @@ if ($isDaemon) {
 				}
 				$store['cyberspark']['tripwire'] = true;
 				if (isset($store['cyberspark']['notifiedtoday'])) {
+					if (!isNotifyHour($properties['notify'])) {
+						// If it's not the 'notify' hour, force the flag false.
+						// This fix can be needed if the daemon was running,
+						//   sent notification, then was shut during DURING the notify hour, 
+						//   but then was restarted during a LATER hour. Obscure, but it 
+						//   will latch the daemon into perpetually rotating the logs every
+						//   time around the loop.
+						$store['cyberspark']['notifiedtoday'] = false;
+					}
 					$properties['notifiedtoday'] = $store['cyberspark']['notifiedtoday'];
 					$prenotified                 = $store['cyberspark']['notifiedtoday'];
 				}
@@ -340,40 +349,65 @@ if ($isDaemon) {
 			
 			///////////////////////////////////////////////////////////////////////////////////
 			///////////////////////////////////////////////////////////////////////////////////
-			// Send properties and/or logs out to administrator
+			// Rotate logs and if asked send properties and/or logs out to administrator
 			$dateTimeNumbers = date('YmdHis');
-			// debugging below
-				$sa = $prenotified?'true':'false';
-				$sb = $postnotified?'true':'false';
-				$sc = $isDaemon?'true':'false';
-				$sd = $properties['sendlogs'];
-			echoIfVerbose("Prenotified: $sa Postnotified: $sb Daemon: $sc Sendlogs: $sd \n");
-			// debugging above
+// debugging below
+//				$sa = $prenotified?'true':'false';
+//				$sb = $postnotified?'true':'false';
+//				$sc = $isDaemon?'true':'false';
+//				$sd = isset($properties['sendlogs'])?$properties['sendlogs']:'undefined';
+//				echoIfVerbose("Prenotified: $sa Postnotified: $sb Daemon: $sc Sendlogs: $sd \n");
+// echo ("Prenotified: $sa Postnotified: $sb Daemon: $sc Sendlogs: $sd \n");
+// debugging above
 			if ((!$prenotified && $postnotified) || !$isDaemon)  {
-				if (isset($properties['sendlogs']) && ($properties['sendlogs'] != null) && (strlen($properties['sendlogs']) > 0)) {
+				// Note that we only do this during the daily notification hour
+				// The 'rotate' keyword causes us to rotate files once a day.
+				// It can be bare, or with a logical value
+				//   rotate
+				// or
+				//   rotate=true
+				//   rotate=false
+				$rotate = isset($properties['rotate']);		// is true if 'rotate' is even present
+				if ($rotate) {
+					// If the keyword is present at all, it is 'true' at this point.
+					// If it contains a "=true" or "=false" value, then we check it
+					if (is_string($properties['rotate']) && (strlen($properties['rotate']) > 0) && ($properties['rotate'] != strtolower('true'))) {
+						$rotate = false;
+					}
+				}
+				// Sendlogs
+				// keyword and value tell where to send rotated logs
+				//   sendlogs=email@example.com
+				$sendLogs = isset($properties['sendlogs']) && ($properties['sendlogs'] != null) && (strlen($properties['sendlogs']) > 0);
+				// Do various things connected with log rotation and/or sending to administrator
+				// Note that we can rotate without sending, but we cannot send without rotating.
+				$rotate = $rotate || $sendLogs;		// rotate if specifically asked for OR if sending logs
+				// So if we are rotating, then there are things to do...
+				if ($rotate) {
 					///////////////////////////////////////////////////////////////////////////////////
 					///////////////////////////////////////////////////////////////////////////////////
 					// Send (a possibly updated) properties file to our admin once a day
 					// It's time to send
 					try {
 						$saveTo = $properties['to'];
-						echoIfVerbose("Props and logs will be sent to $properties[sendlogs] \n");
-						$properties['to'] = $properties['sendlogs'];
-						echoIfVerbose("Send a copy of the properties file \n");
-						$copyFileName = str_replace(PROPS_EXT, '-'.$dateTimeNumbers.PROPS_EXT, $propsFileName);
-						if (copy($propsFileName,$copyFileName)) {
-							sendMailAttachment('Props', 'A copy of the properties file for '.$ID.' is attached.', $properties, $copyFileName);
-							unlink($copyFileName);
+						if ($sendLogs) {
+							echoIfVerbose("Props and logs will be sent to $properties[sendlogs] \n");
+							$properties['to'] = $properties['sendlogs'];
+							echoIfVerbose("Send a copy of the properties file \n");
+							$copyFileName = str_replace(PROPS_EXT, '-'.$dateTimeNumbers.PROPS_EXT, $propsFileName);
+							if (copy($propsFileName,$copyFileName)) {
+								sendMailAttachment('Props', 'A copy of the properties file for '.$ID.' is attached.', $properties, $copyFileName);
+								unlink($copyFileName);
+							}
 						}
-
 						///////////////////////////////////////////////////////////////////////////////////
 						///////////////////////////////////////////////////////////////////////////////////
 						// Send (and rotate) the log file to our admin once a day
 						// It's time to send
 						$copyFileName = str_replace(LOG_EXT, '-'.$dateTimeNumbers.LOG_EXT, $logFileName);
 						$copyFileName .= ZIP_EXT;
-						echoIfVerbose("Rotate and send a copy of the log file \n");
-						// Close my log file
+						echoIfVerbose("Rotating the log file. \n");
+						// Close the log file so it can be zipped.
 						writeLogAlert('Closing the log in order to rotate.');
 						endLog();
 						// gzip the log file
@@ -387,13 +421,17 @@ if ($isDaemon) {
 						// unlink the file that was just zipped
 						// zipped logs are not removed
 						unlink($logFileName);
-						// Open a new log file
+						// Start a new log file
 						beginLog();
-						// Email the zipped log to administrator
-						// See if it should be sent to a special email address
-						//   logs=email@example.com
-						// ...in the properties file
-						sendMimeMail('Log', 'A copy of the log file for '.$ID.' is attached.', $properties, $copyFileName);
+						if ($sendLogs) {
+							echoIfVerbose("Sending the log. \n");
+							writeLogAlert('Sending the log.');
+							// Email the zipped log to administrator
+							// See if it should be sent to a special email address
+							//   logs=email@example.com
+							// ...in the properties file
+							sendMimeMail('Log', 'A copy of the log file for '.$ID.' is attached.', $properties, $copyFileName);
+						}
 					}
 					catch (Exception $zx) {
 						echoIfVerbose('Exception when sending files: '.$zx->getMessage()." \n");
