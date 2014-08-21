464bf4b4d15deb6efdb938256d37ec65649cf7e4
JimSky7 <sky@cyberspark.net>
2014-08-21 00:11:00 -0700

    Add clickable link to analyzed site(s)

--- analysis/index-cs-analysis-common.php
+++ analysis/index-cs-analysis-common.php
@@ -244,7 +244,7 @@ function cs_http_get($url) {
 		else {
 			$getDataURL[$key]	= CS_URL_GET."?format=tsv&URL_HASH=$URL_HASH&pad=true&span=$span";
 		}
-		echo "<svg class='chart' id='H_$URL_HASH' width='$WIDTH_CHART'></svg>\r\n";
+		echo "<svg class='chart' id='H_$URL_HASH' width='$WIDTH_CHART'></svg><span style='size:32pt;'><a href='$sites[$key]' style='text-decoration:none;' target='W_$URL_HASH'>»</a></span>\r\n";
 	}
 
 ////////////////////////////////////////////////////////////////////////
