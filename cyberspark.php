443a471bfb7a474f1b86baf1446b7e3afbad289c
JimSky7 <sky@cyberspark.net>
2014-10-14 00:26:57 -0700

    Better log-transport shutdown

--- cyberspark.php
+++ cyberspark.php
@@ -116,57 +116,6 @@ $logTransportProcess = null;
 // Note that if this process becomes 'unresponsive' the shutdown will not be
 //   performed. Specifically, this could leave a log-transport running.
 function shutdownFunctionWrapper($sig) {
-	global $pipes;
-	global $ID;
-	global $logTransportProcess;
-	global $path;
-	// Terminate log-transport process
-	if ($pipes != null && ($sig === SIGINT || $sig === SIGTERM)) {
-		echo "Shutting down $ID log transport. \n";
-		try {
-			@fclose($pipes[0]);			// note MUST do this or proc_terminate() may fail
-			$LTpidFileName = $path . $ID . '-transport' . PID_EXT;
-			//// First, send SIGINT to the log transport.
-			//   Note that each transport is run using the PHP command line interpreter,
-			//   so it is "enclosed" by an 'sh' shell. What we're doing first is sending
-			//   the SIGINT to the PHP child, not to the 'sh' that surrounds it.
-			if (file_exists($LTpidFileName)) {
-				$pidNumber = file_get_contents($LTpidFileName, PID_FILESIZE_LIMIT);
-				shell_exec ("kill -INT $pidNumber");	// terminate as if CTRL-C
-			}
-			// Pass on any output that came through a pipe from the child process
-			if (isset($pipes[1])) {
-				try {
-					while (($line = fgets($pipes[1])) !== false) {
-						echo $line;
-					}
-				}
-				catch (Exception $x) {
-				}
-			}
-			@fclose($pipes[1]);			// note MUST do this or proc_terminate() may fail
-			@fclose($pipes[2]);			// note MUST do this or proc_terminate() may fail
-			// Terminate the 'sh' process we launched
-			//   Each of these has a child log transport process which has already been
-			//   terminated (see the 'kill' above) and we have flushed the output pipe and
-			//   echoed it to stdout as well.
-			proc_terminate($logTransportProcess, SIGTERM);
-			$i = 10;			// loop maximum of this many times
-			while ($i-- > 0) {
-				$status = proc_get_status($logTransportProcess);
-				if ($status['running']) {
-					usleep(SHUTDOWN_WAIT_TIME);	// wait if process has not stopped
-				}
-				else {
-					break;			// process has stopped. Don't need to wait any more.
-				}
-			}
-			// Formally close the process
-			proc_close($logTransportProcess);
-		}
-		catch (Exception $txp) {
-		}
-	}
 	return shutdownFunction($sig);		// in includes/shutdown.inc
 }
 ///////////////////////////////////////////////////////////////////////////////////
@@ -571,7 +520,8 @@ if (file_exists('log-transport.php')) {
 
 	///////////////////////////////// 
 	// clean up
-	doBeforeExit();	
+	shutdownFunctionWrapper(NORMAL_EXIT);
+//	doBeforeExit();	
 
 	exit;
 
