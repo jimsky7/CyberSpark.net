6d9e5c4f4b9276a8f62c1693fc3282b5c3c55a38
JimSky7 <sky@cyberspark.net>
2015-01-31 14:37:47 -0800

    Put cs_http_get() in one place only

--- analysis/index-cs-analysis-setupdates.php
+++ analysis/index-cs-analysis-setupdates.php
@@ -1,5 +1,7 @@
 <?php
 
+include ('cs-analysis-functions.php');
+
 ////////////////////////////////////////////////////////////////////////
 // Determine whether date is "NOW" or a specific calendar date
 $calendar=false;
@@ -135,29 +137,3 @@ else {
 //	echo "End: " . $dt->format('Y-m-d H:i:s') . "<br/>\r\n";
 //	echo "Start: " . $dt->format('Y-m-d H:i:s') . "<br/>\r\n";
 
-////////////////////////////////////////////////////////////////////////
-function cs_http_get($url) {
-	$result = '';
-	try {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 				$url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER,		true);
-		if(defined('CS_HTTP_USER') && defined('CS_HTTP_PASS')) {
-			// You can define user name and password in cs-log-pw.php
-			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-			curl_setopt($ch, CURLOPT_USERPWD, CS_HTTP_USER.':'.CS_HTTP_PASS);
-		}
-		$curlResult = curl_exec($ch);
-		if ($curlResult === FALSE) {
-			$result = "Error ".curl_errno($ch).": ".curl_error($ch);
-		}
-		else {
-			$result = $curlResult;
-		}
-		curl_close($ch);
-	}
-	catch (Exception $chgx) {
-		$result = '<!-- Exception: '.$chgx->getMessage()." -->\r\n";
-	}
-	return $result;
-}
